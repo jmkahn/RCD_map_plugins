32c54cd4956a58947c21d1bb758af9e375d19026
jmkahn <jmkahn@hmc.edu>
2022-07-08 16:24:14 -0700

    clean up repo a little

--- import_clean_project_json.py
+++ import_clean_project_json.py
@@ -1,6 +1,9 @@
 # code from https://www.delftstack.com/howto/python/python-get-json-from-url/
+# this file was used toget and modify the formatting of the Gold Ridge project data that I got from the Project Tracker webservices 
+# the modified JSON is the one currently used as a sample for Project data 
+# when Sitka updates the webservices, I will do these operations in functions.js 
 
-import requests, json
+import requests
 url = requests.get("https://www.rcdprojects.org/WebServices/GetProjectsByOrganization/JSON/bcdb2d4b-5d7a-4e25-b2e2-7634453c80e8/62")
 text = url.text 
 
@@ -19,5 +22,3 @@ text.replace('\r', ' ')
 # //TODO: double escape internal quotes in description of ProjectID 6343, 6344, 13868 
 # instead I replaced double quotes with single bc json doesn't recognize single quotes (\" -> ')
 text.replace("\"", "'")
-
-# TODO: figure out how to post cleaned JSON to retrieve in map js 
