52317154a178c1c395be284770eb3d791226b2e8
Jenna Kahn <31321933+jmkahn@users.noreply.github.com>
2022-10-28 13:00:47 -0700

    Update attribution note

--- functions.js
+++ functions.js
@@ -73,8 +73,9 @@ function print_map_code(rcd_name, organization_ID, color, current_map_extent){
             //add base layer 
             var tiles = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiamthaG4xNjE4IiwiYSI6ImNsNXZvMnZ3OTBibGYzY3A0cDAwOGhudDYifQ.S7Iz_lKck91Rx9L2mxBNlg', {
                 maxZoom: 18,
-                attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
-                    'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+                attribution: 'Project data retrieved from<a href="https://www.rcdprojects.org">RCD Project Tracker</a>, '+
+                    'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+                    'Imagery &copy;<a href="https://www.mapbox.com/">Mapbox</a>',
                 id: 'mapbox/streets-v11',
                 tileSize: 512,
                 zoomOffset: -1
